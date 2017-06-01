2d3740c105abbc338ae50a8b1e0b9e2a9ab6e6e6
Zoran Todorovic <zoran@arsfutura.co>
2017-06-01 19:40:14 +0200

    Init.

---
+++ CustomURLSchemeApp/CustomURLSchemeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomURLSchemeApp
+//
+//  Created by Zoran Todorovic on 01/06/2017.
+//  Copyright © 2017 Zoran Todorovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
